7dfd6083a505f6542c474f59493a55e514a3f089
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-11-04 18:48:06 -0300

    Login

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,6 +1,11 @@
 import { Link, NavLink } from "react-router-dom";
+import { Badge } from "@mui/material";
+import PropTypes from "prop-types";
 
-export const NavBar = () => {
+import { useSelector } from "react-redux/es/hooks/useSelector";
+
+export const NavBar = ({ logged }) => {
+  const orderProduct = useSelector((state) => state.order.purcheseOrder);
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
@@ -21,19 +26,41 @@ export const NavBar = () => {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <NavLink to="/Products" className="nav-link ">
-                Home
+              <NavLink to="/Tienda" className="nav-link ">
+                Tienda
               </NavLink>
             </li>
 
-            <li className="nav-item">
-              <NavLink to="/" className="nav-link">
-                Log in
-              </NavLink>
-            </li>
+            {logged && (
+              <li className="nav-item">
+                <NavLink to="/Pedidos" className="nav-link">
+                  Pedidos
+                </NavLink>
+              </li>
+            )}
+            {logged && (
+              <li className="nav-item">
+                <NavLink to="/Carrito">
+                  <Badge badgeContent={orderProduct.length} color="primary">
+                    🛒
+                  </Badge>
+                </NavLink>
+              </li>
+            )}
+            {!logged && (
+              <li className="nav-item">
+                <NavLink to="/" className="nav-link">
+                  Log in
+                </NavLink>
+              </li>
+            )}
           </ul>
         </div>
       </div>
     </nav>
   );
+};
+
+NavBar.prototype = {
+  logged: PropTypes.bool,
 };
