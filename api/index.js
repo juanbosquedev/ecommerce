4c85fb76784f9870f7dd56b9772839ef4da780c4
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-11-07 17:10:52 -0300

    models relationated / fake users.json

--- api/index.js
+++ api/index.js
@@ -1,12 +1,16 @@
 require("dotenv").config();
 const server = require("./src/server");
 const { conn } = require("./src/database/database");
+const { Users, Purchases } = require("./src/models");
+
+Users.hasMany(Purchases, { foreignKey: "usuario_purchase", sourceKey: "id" });
+Purchases.belongsTo(Users, { foreignKey: "usuario_purchase", targetKey: "id" });
 
 const { PORT } = process.env;
 
 async function main() {
   try {
-    await conn.authenticate();
+    await conn.sync();
     console.log("Connection has been established successfully.");
     server.listen(PORT, () => {
       console.log(`Server listening on port: ${PORT}`);
