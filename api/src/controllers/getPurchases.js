2c565756efc0236213c4f7c039d2b5705ac46ee6
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-11-08 12:07:16 -0300

    handling answer

--- api/src/controllers/getPurchases.js
+++ api/src/controllers/getPurchases.js
@@ -1,9 +1,10 @@
-const { Purchases, Users } = require("../models");
+const { Purchases } = require("../models");
 
 module.exports = async (req, res) => {
   try {
     const allPurchases = await Purchases.findAll();
     if (allPurchases.length > 0) res.status(200).json(allPurchases);
+   else res.status(200).send("Esperamos por tu pedido")
   } catch (error) {
     res.status(506).json({ message: "Error interno del servidor" }, error);
   }
