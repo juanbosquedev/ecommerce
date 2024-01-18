f171de170e7c20044c4dd87b2f5baafd607f46e4
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2024-01-18 09:09:15 -0300

    tidying code

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -13,11 +13,9 @@ const router = Router();
 
 router.post("/userLogin", userLogin);
 router.post("/userPurchase", userPurchase);
-
 router.get("/productsAll", productsAll);
 router.get("/userPurchase/:userID", purchaseById);
 router.get("/getPurchases", getPurchases);
-
 router.delete("/userPurchase/:id", deleteUserPurchase);
 
 module.exports = router;
