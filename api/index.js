afd9f2f471865a88eeac612a771268916d35ddf9
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2024-03-07 23:38:02 -0300

    s

--- api/index.js
+++ api/index.js
@@ -1,19 +1,35 @@
-require("dotenv").config();
-const server = require("./src/server");
-const { conn } = require("./src/database/database");
+// require("dotenv").config();
+// const server = require("./src/server");
+// const { conn } = require("./src/database/database");
 
+// const { PORT } = process.env;
+
+// async function main() {
+//   try {
+//     await conn.sync();
+//     console.log("Connection has been established successfully.");
+//     server.listen(PORT, () => {
+//       console.log(`Server listening on port: ${PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("Unable to connect to the database:", error);
+//   }
+// }
+
+// main();
+
+require("dotenv").config();
+const express = require('express');
+
+const app = express();
 const { PORT } = process.env;
 
-async function main() {
-  try {
-    await conn.sync();
-    console.log("Connection has been established successfully.");
-    server.listen(PORT, () => {
-      console.log(`Server listening on port: ${PORT}`);
-    });
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-}
+// Ruta para responder "Hola"
+app.get('/', (req, res) => {
+  res.send('Hola');
+});
 
-main();
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
