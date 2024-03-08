20e1368fb3f7b740481306d60b6507d5f99345aa
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2024-03-07 23:09:28 -0300

    S

--- api/src/server.js
+++ api/src/server.js
@@ -1,30 +1,40 @@
-const express = require("express");
-const morgan = require("morgan");
+// const express = require("express");
+// const morgan = require("morgan");
 
-const server = express();
+// const server = express();
 
-server.use(express.json());
-server.use(morgan("dev"));
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
-server.use(require("./routes/index"));
-server.use("*", (req, res)=>{
-  res.status(404).send("Not Found")
-});
-server.use((err, req, res, next) => {
-res.status(err.statusCode || 500).send({
-  error: true,
-  message: err.message,
-});
+// server.use(express.json());
+// server.use(morgan("dev"));
+// server.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
+// server.use(require("./routes/index"));
+// server.use("*", (req, res)=>{
+//   res.status(404).send("Not Found")
+// });
+// server.use((err, req, res, next) => {
+// res.status(err.statusCode || 500).send({
+//   error: true,
+//   message: err.message,
+// });
+// });
+
+// module.exports = server;
+
+const express = require('express');
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hola');
 });
 
-module.exports = server;
-
+module.exports = app;
