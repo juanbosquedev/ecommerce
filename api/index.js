8b9988e602e453e0c7610c56131f9135b808837d
Juan Bosque <86373520+juanbosquedev@users.noreply.github.com>
2024-03-07 23:50:42 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,35 +1,19 @@
-// require("dotenv").config();
-// const server = require("./src/server");
-// const { conn } = require("./src/database/database");
-
-// const { PORT } = process.env;
-
-// async function main() {
-//   try {
-//     await conn.sync();
-//     console.log("Connection has been established successfully.");
-//     server.listen(PORT, () => {
-//       console.log(`Server listening on port: ${PORT}`);
-//     });
-//   } catch (error) {
-//     console.error("Unable to connect to the database:", error);
-//   }
-// }
-
-// main();
-
 require("dotenv").config();
-const express = require('express');
+const server = require("./src/server");
+const { conn } = require("./src/database/database");
 
-const app = express();
 const { PORT } = process.env;
 
-// Ruta para responder "Hola"
-app.get('/', (req, res) => {
-  res.send('Hola');
-});
+async function main() {
+  try {
+    await conn.sync();
+    console.log("Connection has been established successfully.");
+    server.listen(PORT, () => {
+      console.log(`Server listening on port: ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
 
-// Iniciar el servidor
-app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
-});
+main();
